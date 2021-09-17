1fd8caf6c7f2b1cb7b3bb3d841b816c0a33dcc50
murillojoaquim <murillojalves@hotmail.com>
2021-09-17 18:43:26 -0300

    calculo medidas

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Digite um valor em Metros para converter para KM e CM:");
+
+            string metros = Console.ReadLine();
+            double cmMetro = Convert.ToDouble(metros) * 100;
+            double kmMetro = Convert.ToDouble(metros) / 1000;
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("\nEquivalência:");
+
+            Console.WriteLine($"{cmMetro} cm \n{metros} m \n{kmMetro} km");
+
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+    }
+}
